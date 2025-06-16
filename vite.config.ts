02db111eed2c900f195a66787987702da0ebc837
Jesus <requenaslg@gmail.com>
2025-06-15 23:38:23 -0400

    build: github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/calculadora_propinas/",
   plugins: [
     react(),
     tailwindcss(),]
